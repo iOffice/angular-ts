2ed32e629cf6ca256f8a7fc8cc0b74080d452883
Manuel Lopez <jmlopez@iofficecorp.com>
2016-07-14 18:06:12 -0500

    doing a single pre-release

--- build/pre-publish.sh
+++ build/pre-publish.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-VERSION=$(node --eval "console.log(require('./package.json').version);")
+git checkout pre-release
+git fetch --prune
+git reset --hard origin/master
 
-git checkout -b pre-release-v$VERSION
-
-npm run tsc
+bash build/transpile.sh || exit 0
 git add lib/ -f
 
-git commit -m "v$VERSION"
-git push -u origin pre-release-v$VERSION
+git commit -m "pre-release"
+git push origin pre-release -f
 
 git checkout master
+echo '[DONE]'
