afdacbbbda079cfce18166de8df304012a1e1365
Manuel Lopez <jmlopez@iofficecorp.com>
2016-10-17 15:39:26 -0500

    [chore:pre-release] building pre-release script

--- build-tools/pre-release.ts
+++ build-tools/pre-release.ts
@@ -1,7 +1,27 @@
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
+import 'colors';
 
-exec('git checkout pre-release', (err) => {
-  if (err) {
-    return process.stderr.write(err.toString());
+function cout(msg: string): void {
+  process.stdout.write(msg);
+}
+
+function exit(code: number): void {
+  process.exit(code);
+}
+
+function run(cmd: string, callback?: Function): void {
+  const output: Buffer = execSync(cmd);
+  if (callback) {
+    callback(output.toString());
+  }
+}
+
+run('git status -s', (stdout: string) => {
+  if (stdout) {
+    cout(`${'ERROR:'.red} There are uncommitted changes:\n`);
+    cout(stdout);
+    exit(0);
   }
 });
+
+run('node ./build/transpile.js build-tools');
