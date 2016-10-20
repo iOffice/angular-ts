a10f0b47f85a7edcb7ca88de9427dcd9c80ac2b4
Manuel Lopez <jmlopez@iofficecorp.com>
2016-10-20 16:23:30 -0500

    [chore:pre-test] getting things ready to write the hook

--- build-tools/ts-hook.ts
+++ build-tools/ts-hook.ts
@@ -1,7 +1,9 @@
-function run(): void {
-  console.log('Ran...');
+import { run } from 'ts-publish';
+
+function hook(): void {
+  run('ts-compile angular-ts --config build-tools/ts-publish');
 }
 
 export {
-  run,
+  hook,
 }
