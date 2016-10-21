be04dcadedffae270ebe48a36e4cdf373f71f64f
Manuel Lopez <jmlopez@iofficecorp.com>
2016-10-21 18:24:26 -0500

    [chore:ts-publish-hook] preparing for pre-release
    The hook compiles all the files into build/lib and then moves the
    contents the root folder. Then they get added to git.

--- build-tools/ts-hook.ts
+++ build-tools/ts-hook.ts
@@ -1,7 +1,33 @@
-import { run } from 'ts-publish';
+import {
+  IFileMessages,
+  getConfig,
+  parseTsPublishConfig,
+  compile,
+  formatResults,
+  move,
+  run,
+} from 'ts-publish';
+import * as _ from 'lodash';
 
 function hook(): void {
-  run('ts-compile angular-ts --config build-tools/ts-publish');
+  const project = parseTsPublishConfig('./build-tools/ts-publish')[0];
+  const lintOptions: any = getConfig('tslint');
+
+  const results = compile(project, project.compilerOptions, lintOptions);
+  let numMessages: number = 0;
+  _.each(results, (file: IFileMessages) => {
+    numMessages += file.messages.length;
+  });
+
+  if (numMessages) {
+    process.stderr.write(formatResults(results));
+    // throw Error('messages found');
+  }
+
+  const files = move('build/lib/*', '.');
+  _.each(files, (file) => {
+    run(`git add ${file} -f`);
+  });
 }
 
 export {
