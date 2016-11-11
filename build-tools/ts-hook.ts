49f8f87c1e6ee9ed7ccd9c7366bec480fd81f020
Manuel Lopez <jmlopez@iofficecorp.com>
2016-11-11 15:26:48 -0600

    [chore] updating ts-hook file

--- build-tools/ts-hook.ts
+++ build-tools/ts-hook.ts
@@ -3,22 +3,45 @@ import {
   move,
   run,
   compileProject,
+  cout,
+  changePackageVersion,
+  pushTags,
 } from 'ts-publish';
-import * as _ from 'lodash';
 
-function hook(): void {
+function hook(action: string, options?: any): void {
+  const target = options ? options.target : '';
   const projectResult = compileProject('angular-ts', './build-tools/ts-publish', true);
   if (projectResult.numMessages) {
     process.stderr.write(formatResults(projectResult.results));
-    // throw Error('messages found');
+    throw Error('messages found');
   }
 
-  const files = move('build/lib/*', '.');
-  _.each(files, (file) => {
-    run(`git add ${file} -f`);
-  });
+  if (action === 'trial') {
+    move('build/lib/', target);
+  } else {
+    const files = move('build/lib/', '.');
+    files.forEach((file) => {
+      run(`git add ${file} -f`);
+    });
+  }
+}
+
+function publish(action: string, version: string): void {
+  const date = new Date();
+  const finalVersion = action === 'pre-release' ? `${version}-beta.${date.valueOf()}` : version;
+  cout(`Publishing version ${finalVersion}\n`);
+  if (action === 'pre-release') {
+    changePackageVersion(finalVersion);
+    run(`git commit -m "[pre-release] v${finalVersion}"`);
+    run('npm publish --tag next');
+  } else {
+    run(`git commit -m "[release] v${finalVersion}"`);
+    run('npm publish --tag latest');
+  }
+  pushTags(`v${finalVersion}`);
 }
 
 export {
   hook,
+  publish,
 }
