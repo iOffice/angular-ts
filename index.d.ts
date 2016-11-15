7854ed829c1108f473012c4a6913d1cf1b0fdf44
Manuel Lopez <jmlopez@iofficecorp.com>
2016-11-15 17:14:32 -0600

    [release] v0.2.1

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import { Inject, inject } from './injectable';
+import { Directive } from './directive';
+import { ngRegister } from './ng-register';
+import { mix, isinstance } from './mix';
+import { loadNgModule } from './loadNgModule';
+export { inject, Inject, Directive, ngRegister, mix, isinstance, loadNgModule };
---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+var injectable_1 = require("./injectable");
+exports.Inject = injectable_1.Inject;
+exports.inject = injectable_1.inject;
+var directive_1 = require("./directive");
+exports.Directive = directive_1.Directive;
+var ng_register_1 = require("./ng-register");
+exports.ngRegister = ng_register_1.ngRegister;
+var mix_1 = require("./mix");
+exports.mix = mix_1.mix;
+exports.isinstance = mix_1.isinstance;
+var loadNgModule_1 = require("./loadNgModule");
+exports.loadNgModule = loadNgModule_1.loadNgModule;
