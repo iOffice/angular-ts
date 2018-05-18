ebc10d36484443af252a79d2de84b78208a8b4bd
Manuel Lopez <jmlopez@iofficecorp.com>
2018-05-18 11:39:56 -0500

    [feat] adding loadNgModuleNoDefault
    No breaking changes

--- src/main/loadNgModule.ts
+++ src/main/loadNgModule.ts
@@ -20,6 +20,23 @@ function loadNgModule(callback: any): any[] {
   return ['$q', '$ocLazyLoad', ($q: ng.IQService, $ocLazyLoad: any) => {
     return $q((resolve: Function) => {
       callback((file: any) => {
+        const module: any = file.default;
+        $ocLazyLoad.load({ name: module.name });
+        resolve(module.name);
+      });
+    });
+  }];
+}
+
+/**
+ * Does the exact same thing as `loadNgModule` with the exception that it requires the module
+ * file to export a single NgModule object with its name ending in `Module`. This is for
+ * files that follow the convention of not using default exports.
+ */
+function loadNgModuleNoDefault(callback: any): any[] {
+  return ['$q', '$ocLazyLoad', ($q: ng.IQService, $ocLazyLoad: any) => {
+    return $q((resolve: Function) => {
+      callback((file: any) => {
         const modules = Object.keys(file).filter(key => key.endsWith('Module'));
         if (modules.length !== 1) {
           throw new Error('Only one angularJS module should be exported in a lazy module');
@@ -35,4 +52,5 @@ function loadNgModule(callback: any): any[] {
 
 export {
   loadNgModule,
+  loadNgModuleNoDefault,
 };
